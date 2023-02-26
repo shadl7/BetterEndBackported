896d144686cdcdd6c0d4050a06e52d5e5fe71eb7
Kasualix <670067575@qq.com>
2023-02-26 14:27:32 +0800

    bfendcity

---
+++ src/main/java/mod/beethoven92/betterendforge/mixin/EndCityBridgeMixin.java
@@ -0,0 +1,72 @@
+package mod.beethoven92.betterendforge.mixin;
+
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.gen.feature.structure.EndCityPieces;
+import net.minecraft.world.gen.feature.structure.StructurePiece;
+import net.minecraft.world.gen.feature.template.TemplateManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.List;
+import java.util.Random;
+
+@Pseudo
+@Mixin(targets = "net.minecraft.world.gen.feature.structure.EndCityPieces$3")
+public class EndCityBridgeMixin {
+    @Shadow
+    public boolean shipCreated;
+
+    public int shipsCreated;
+
+    /**
+     * @author Doctor Who
+     * @reason Saving The World
+     */
+    @Overwrite
+    public boolean generate(TemplateManager p_71180_, int p_71181_, EndCityPieces.CityTemplate p_71182_, BlockPos p_71183_, List<StructurePiece> p_71184_, Random p_71185_) {
+        Rotation rotation = p_71182_.placeSettings.getRotation();
+
+        int i = p_71185_.nextInt(4) + 1;
+        i += Math.max(0, (6 - (p_71181_ / 2)));
+
+        EndCityPieces.CityTemplate endcitypieces$endcitypiece = EndCityPieces.addHelper(p_71184_, EndCityPieces.addPiece(p_71180_, p_71182_, new BlockPos(0, 0, -4), "bridge_piece", rotation, true));
+        endcitypieces$endcitypiece.componentType = -1;
+        int j = 0;
+
+        for(int k = 0; k < i; ++k) {
+            if (p_71185_.nextBoolean()) {
+                endcitypieces$endcitypiece = EndCityPieces.addHelper(p_71184_, EndCityPieces.addPiece(p_71180_, endcitypieces$endcitypiece, new BlockPos(0, j, -4), "bridge_piece", rotation, true));
+                j = 0;
+            } else {
+                if (p_71185_.nextBoolean()) {
+                    endcitypieces$endcitypiece = EndCityPieces.addHelper(p_71184_, EndCityPieces.addPiece(p_71180_, endcitypieces$endcitypiece, new BlockPos(0, j, -4), "bridge_steep_stairs", rotation, true));
+                } else {
+                    endcitypieces$endcitypiece = EndCityPieces.addHelper(p_71184_, EndCityPieces.addPiece(p_71180_, endcitypieces$endcitypiece, new BlockPos(0, j, -8), "bridge_gentle_stairs", rotation, true));
+                }
+
+                j = 4;
+            }
+        }
+
+        if (shipsCreated <= 3 && p_71185_.nextInt(7) == 0) {
+            shipCreated = false;
+            shipsCreated++;
+        }
+
+        if (!shipCreated && p_71185_.nextInt(6 - Math.min(5, p_71181_)) == 0) {
+            EndCityPieces.addHelper(p_71184_, EndCityPieces.addPiece(p_71180_, endcitypieces$endcitypiece, new BlockPos(-8 + p_71185_.nextInt(8), j, -70 + p_71185_.nextInt(10)), "ship", rotation, true));
+            this.shipCreated = true;
+            shipsCreated++;
+
+        } else if (!EndCityPieces.recursiveChildren(p_71180_, EndCityPieces.HOUSE_TOWER_GENERATOR, p_71181_ + 1, endcitypieces$endcitypiece, new BlockPos(-3, j + 1, -11), p_71184_, p_71185_)) {
+            return false;
+        }
+
+        endcitypieces$endcitypiece = EndCityPieces.addHelper(p_71184_, EndCityPieces.addPiece(p_71180_, endcitypieces$endcitypiece, new BlockPos(4, j, 0), "bridge_end", rotation.getRotated(Rotation.CLOCKWISE_180), true));
+        endcitypieces$endcitypiece.componentType = -1;
+        return true;
+    }
+}
---
+++ src/main/java/mod/beethoven92/betterendforge/mixin/EndCityPiecesMixin.java
@@ -0,0 +1,63 @@
+package mod.beethoven92.betterendforge.mixin;
+
+import net.minecraft.util.Rotation;
+import net.minecraft.util.Tuple;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.gen.feature.structure.EndCityPieces;
+import net.minecraft.world.gen.feature.structure.StructurePiece;
+import net.minecraft.world.gen.feature.template.TemplateManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.List;
+import java.util.Random;
+
+@Mixin(targets = "net.minecraft.world.gen.feature.structure.EndCityPieces$2")
+public class EndCityPiecesMixin {
+
+    @Redirect(at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 3), method = "generate")
+    public int getCloudHeight(Random instance, int bound) {
+        return instance.nextInt(bound + 2);
+    }
+
+    /**
+     * @author Doctor Who
+     * @reason Saving The World
+     */
+    @Overwrite
+    public boolean generate(TemplateManager p_71170_, int p_71171_, EndCityPieces.CityTemplate p_71172_, BlockPos p_71173_, List<StructurePiece> p_71174_, Random p_71175_) {
+        Rotation rotation = p_71172_.placeSettings.getRotation();
+        EndCityPieces.CityTemplate $$7 = EndCityPieces.addHelper(p_71174_, EndCityPieces.addPiece(p_71170_, p_71172_, new BlockPos(3 + p_71175_.nextInt(2), -3, 3 + p_71175_.nextInt(2)), "tower_base", rotation, true));
+        $$7 = EndCityPieces.addHelper(p_71174_, EndCityPieces.addPiece(p_71170_, $$7, new BlockPos(0, 7, 0), "tower_piece", rotation, true));
+        EndCityPieces.CityTemplate endcitypieces$endcitypiece1 = p_71175_.nextInt(6) == 0 ? $$7 : null;
+        int i = 1 + p_71175_.nextInt(3);
+
+        for(int j = 0; j < i; ++j) {
+            $$7 = EndCityPieces.addHelper(p_71174_, EndCityPieces.addPiece(p_71170_, $$7, new BlockPos(0, 4, 0), "tower_piece", rotation, true));
+            if (j < i - 1 && p_71175_.nextInt(3) == 0) {
+                endcitypieces$endcitypiece1 = $$7;
+            }
+        }
+
+        if (endcitypieces$endcitypiece1 != null) {
+            for(Tuple<Rotation, BlockPos> tuple : EndCityPieces.TOWER_BRIDGES) {
+                if (p_71175_.nextBoolean()) {
+                    EndCityPieces.CityTemplate endcitypieces$endcitypiece2 = EndCityPieces.addHelper(p_71174_, EndCityPieces.addPiece(p_71170_, endcitypieces$endcitypiece1, tuple.getB(), "bridge_end", rotation.getRotated(tuple.getA()), true));
+                    EndCityPieces.recursiveChildren(p_71170_, EndCityPieces.TOWER_BRIDGE_GENERATOR, p_71171_ + 1, endcitypieces$endcitypiece2, (BlockPos)null, p_71174_, p_71175_);
+                }
+            }
+
+            EndCityPieces.addHelper(p_71174_, EndCityPieces.addPiece(p_71170_, $$7, new BlockPos(-1, 4, -1), "tower_top", rotation, true));
+        } else {
+            if (p_71171_ < 15) {
+                return EndCityPieces.recursiveChildren(p_71170_, EndCityPieces.FAT_TOWER_GENERATOR, p_71171_ + 1, $$7, (BlockPos)null, p_71174_, p_71175_);
+            }
+
+            EndCityPieces.addHelper(p_71174_, EndCityPieces.addPiece(p_71170_, $$7, new BlockPos(-1, 4, -1), "tower_top", rotation, true));
+        }
+
+        return true;
+    }
+}
