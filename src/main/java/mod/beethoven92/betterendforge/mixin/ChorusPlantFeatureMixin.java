ac088550a5ec47562b855d08d3b4ad3465dbbe54
Kasualix <670067575@qq.com>
2023-03-04 22:29:30 +0800

    remove invalid 384, try to fix chorus plant

--- src/main/java/mod/beethoven92/betterendforge/mixin/ChorusPlantFeatureMixin.java
+++ src/main/java/mod/beethoven92/betterendforge/mixin/ChorusPlantFeatureMixin.java
@@ -1,12 +1,12 @@
 package mod.beethoven92.betterendforge.mixin;
 
+
+import mod.beethoven92.betterendforge.common.block.BlockProperties;
 import mod.beethoven92.betterendforge.common.init.ModBlocks;
-import mod.beethoven92.betterendforge.common.util.BlockHelper;
-import mod.beethoven92.betterendforge.common.util.ModMathHelper;
+import mod.beethoven92.betterendforge.common.world.generator.GeneratorOptions;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.ChorusFlowerBlock;
-import net.minecraft.block.SixWayBlock;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ISeedReader;
 import net.minecraft.world.gen.ChunkGenerator;
@@ -17,25 +17,22 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Objects;
 import java.util.Random;
 
 @Mixin(ChorusPlantFeature.class)
-public abstract class ChorusPlantFeatureMixin 
-{
-	@Inject(method = "generate*", at = @At("HEAD"), cancellable = true)
-	private void be_generate(ISeedReader worldIn, ChunkGenerator chunkGenerator, Random random, 
-			BlockPos blockPos, NoFeatureConfig config, CallbackInfoReturnable<Boolean> info) 
-	{
-
-		if (worldIn.isAirBlock(blockPos) && worldIn.getBlockState(blockPos.down()).isIn(ModBlocks.CHORUS_NYLIUM.get())) {
-			ChorusFlowerBlock.generatePlant(worldIn, blockPos, random, ModMathHelper.randRange(8, 16, random));
-			BlockState bottom = worldIn.getBlockState(blockPos);
-			if (bottom.isIn(Blocks.CHORUS_PLANT)) {
-				BlockHelper.setWithoutUpdate(
-						worldIn,
-						blockPos,
-						bottom.with(SixWayBlock.DOWN, true)
-				);
+public class ChorusPlantFeatureMixin {
+	@Inject(method = "generate", at = @At("HEAD"), cancellable = true)
+	private void be_generate(ISeedReader structureWorldAccess, ChunkGenerator chunkGenerator, Random random, BlockPos blockPos, NoFeatureConfig defaultFeatureConfig, CallbackInfoReturnable<Boolean> info) {
+		if (structureWorldAccess.isAirBlock(blockPos) && structureWorldAccess.getBlockState(blockPos.down()).isIn(ModBlocks.CHORUS_NYLIUM.get())) {
+			ChorusFlowerBlock.generatePlant(structureWorldAccess, blockPos, random, (8 + random.nextInt(9)));
+			BlockState bottom = structureWorldAccess.getBlockState(blockPos);
+			if (!Objects.requireNonNull(bottom.getBlock().getRegistryName()).toString().equals("minecraft:chorus_plant")) {
+				if (GeneratorOptions.changeChorusPlant()) {
+					structureWorldAccess.getWorld().setBlockState(blockPos, bottom.with(BlockProperties.ROOTS, true).with(BlockStateProperties.DOWN, true));
+				} else {
+					structureWorldAccess.getWorld().setBlockState(blockPos, bottom.with(BlockStateProperties.DOWN, true));
+				}
 			}
 			info.setReturnValue(true);
 		}
